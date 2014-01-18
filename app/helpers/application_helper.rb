e7ea128848bd9e5792f47d5aeb630937bb674672
Miles Starkenburg <milesstarkenburg@gmail.com>
2014-01-18 12:06:18 -0800

    wip

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,43 @@
 module ApplicationHelper
+
+  def title(title = nil)
+    if title.present?
+      content_for :title, title
+    else
+      content_for?(:title) ? APP_CONFIG['default_title'] +  ' | ' + content_for(:title) : APP_CONFIG['default_title']
+    end
+  end
+
+  def meta_keywords(tags = nil)
+    if tags.present?
+      content_for :meta_keywords, tags
+    else
+      content_for?(:meta_keywords) ? [content_for(:meta_keywords), APP_CONFIG['meta_keywords']].join(', ') : APP_CONFIG['meta_keywords']
+    end
+  end
+
+  def meta_description(desc = nil)
+    if desc.present?
+      content_for :meta_description, desc
+    else
+      content_for?(:meta_description) ? content_for(:meta_description) : APP_CONFIG['meta_description']
+    end
+  end
+
+  def meta_viewport(view = nil)
+    if view.present?
+      content_for :meta_viewport, view
+    else
+      content_for?(:meta_viewport) ? content_for(:meta_viewport) : APP_CONFIG['meta_viewport']
+    end
+  end
+
+  def meta_author(author = nil)
+    if author.present?
+      content_for :meta_author, author
+    else
+      content_for?(:meta_author) ? content_for(:meta_author) : APP_CONFIG['meta_author']
+    end
+  end
+
 end
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Portfolio::Application.initialize!
+
+# Look for config.yml system defaults
+APP_CONFIG = YAML.load_file("#{Rails.root}/config/config.yml")
+
