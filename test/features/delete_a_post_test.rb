ad07a70570eb1bf207224b42774baf89ed25029e
Miles Starkenburg <milesstarkenburg@gmail.com>
2014-01-23 09:54:52 -0800

    Skip Delete Test for production app

--- test/features/delete_a_post_test.rb
+++ test/features/delete_a_post_test.rb
@@ -2,8 +2,10 @@ require "test_helper"
 
 feature "Deleting a Post" do
   scenario "A post is deleted with a click" do
+    skip("Don't Want Delete on my production end")
     visit posts_path
 
+    save_and_open_page
     page.find("a[data-confirm][href='#{post_path(posts(:two))}']").click
     page.wont_have_content posts(:two).body
   end
